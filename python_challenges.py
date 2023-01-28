82c332b7814f2dd5379d58854ba808dbb9335d05
Matthew <subinmatt@gmail.com>
2023-01-28 14:38:17 -0800

    cleared extra lines

--- python_challenges.py
+++ python_challenges.py
@@ -1,11 +1,11 @@
 # 1. Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
 
-# num = int(input( "enter a integer: " ))
-# sum_to=num
+num = int(input( "enter a integer: " ))
+sum_to=num
 
-# for i in range(1, sum_to): 
-#     sum_to += i
-#     print(sum_to)
+for i in range(1, sum_to): 
+    sum_to += i
+    print(sum_to)
 
 # sum_to(6)  # returns 21
 # sum_to(10) # returns 55
@@ -27,24 +27,6 @@ print(largestNum([10, 4, 2, 231, 91, 54]))
 
 # 3. Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
 
-# Only gave me one letter
-
-# def occurences (stringA, stringB):
-#     count = 0
-#     for doubleword in stringA:
-#         for letter in doubleword:
-#             if letter == stringB:
-#                 count = count + 1
-#     return count # Had this one tab over originally and code returned an incorrect value
-# stringA = input("stringA: ")
-# stringB = input("stringB: ")
-
-# occurence = occurences(stringA,stringB)
-
-# print(occurence)
-
-#------------
-
 # This method still only gave me one letter
 
 # def occurence (stringA, StringB):
@@ -58,30 +40,6 @@ print(largestNum([10, 4, 2, 231, 91, 54]))
 # stringA = input("stringA: ")
 # stringB = input("stringB: ")
 # print(occurence(stringA, stringB))
-
-#----------
-
-#Final
-
-# def occurence(stringA, stringB):
-#     word = len(stringA)
-#     search = len(stringB)
-#     count = 0
-
-#     for i in range(search - word + 1):
-#         j = 0
-#         while(j < word):
-#             if(stringB[i + j] != stringA[j]):
-#                 break
-#             j += 1
-#         if (j == word):
-#             count += 1
-#             j = 0
-#     return count 
-# stringA = input("stringA: ")
-# stringB = input("stringB: ")
-
-# print(occurence(stringB, stringA))
 
 #-----------
 
@@ -109,9 +67,3 @@ print(product(4, 0.5, 5))
 # product(-1, 4) # returns -4
 # product(2, 5, 5) # returns 50
 # product(4, 0.5, 5) # returns 10.0
-
-
-
-
-
-

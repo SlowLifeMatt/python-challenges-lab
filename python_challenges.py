0f2d4683ec0374de7d66230412d8d6e4d9fb9264
Matthew <subinmatt@gmail.com>
2023-01-28 13:54:28 -0800

    multiple solutions to question 3

--- python_challenges.py
+++ python_challenges.py
@@ -22,6 +22,83 @@ def largestthat( list ):
     return largest
 print(largestthat([10, 4, 2, 231, 91, 54]))
 
-# largest([1, 2, 3, 4, 0])  # returns 4
-# largest([10, 4, 2, 231, 91, 54])  # returns 231
+# largestthat([1, 2, 3, 4, 0])  # returns 4
+# largestthat([10, 4, 2, 231, 91, 54])  # returns 231
+
+# 3. Write a function named occurancesthat takes two string arguments as input and counts the number of occurances of the second string inside the first string.
+
+# Only gave me one letter
+
+# def occurences (stringA, stringB):
+#     count = 0
+#     for doubleword in stringA:
+#         for letter in doubleword:
+#             if letter == stringB:
+#                 count = count + 1
+#     return count # Had this one tab over originally and code returned an incorrect value
+# stringA = input("stringA: ")
+# stringB = input("stringB: ")
+
+# occurence = occurences(stringA,stringB)
+
+# print(occurence)
+
+#------------
+
+# This method still only gave me one letter
+
+# def occurence (stringA, StringB):
+#     count = 0
+
+#     for i in range(len(stringA)):
+#         if (stringA[i] == StringB):
+#             count = count + 1
+#     return count
+
+# stringA = input("stringA: ")
+# stringB = input("stringB: ")
+# print(occurence(stringA, stringB))
+
+#----------
+
+#Final
+
+# def occurence(stringA, stringB):
+#     word = len(stringA)
+#     search = len(stringB)
+#     count = 0
+
+#     for i in range(search - word + 1):
+#         j = 0
+#         while(j < word):
+#             if(stringB[i + j] != stringA[j]):
+#                 break
+#             j += 1
+#         if (j == word):
+#             count += 1
+#             j = 0
+#     return count 
+# stringA = input("stringA: ")
+# stringB = input("stringB: ")
+
+# print(occurence(stringB, stringA))
+
+#-----------
+
+#Okay maybe one is my final
+
+stringA = input("stringA: ")
+stringB = input("stringB: ")
+occurence = 0
+for count in range(len(stringA) - len(stringB) + 1):
+    if(stringA[count:count+len(stringB)] == stringB):
+        occurence += 1
+print(occurence)
+
+
+
+
+
+
+
 
